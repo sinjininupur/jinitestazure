4ea584468b145e388ebaa9810b38f986d7a54a40
SinjiniRoy <sinjiroy@in.ibm.com>
2020-06-24 23:13:24 +0530

    Added my project

--- angular-test/src/app/shared/api.service.ts
+++ angular-test/src/app/shared/api.service.ts
@@ -10,7 +10,7 @@ import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http
 
 export class ApiService {
 
-  endpoint: string = 'http://localhost:8000/api';
+  endpoint: string = 'https://backendjini.azurewebsites.net/api';
   headers = new HttpHeaders().set('Content-Type', 'application/json');
 
   constructor(private http: HttpClient) { }
